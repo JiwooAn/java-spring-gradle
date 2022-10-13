c53468866542d027775dc09be1977569d291c635
JiwooAn <jiyouan9@gmail.com>
2022-10-13 16:09:00 +0900

    feat: static 페이지 만들기

--- src/main/java/web/testController.java
+++ src/main/java/web/testController.java
@@ -11,19 +11,18 @@ import service.TestService;
 public class testController {
 
     @ResponseBody // Controller에서는 객체가 아니라 view를 반환하기 때문에 ResponseBody를 사용해서 객체를 보내준다.
-    @GetMapping("/test")
+    @GetMapping("test")
     public String test(Model model) {
         String result = TestService.test();
         System.out.printf("result ==> %s", result);
         return result;
     }
     
-    @GetMapping("/hello")
+    @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("test", "예오예");
         model.addAttribute("test2", "오예오");
-        // System.out.printf("test ==>", test);
-        // result = testService.test();
+        System.out.printf("model ==> %s", model);
         
         return "hello"; // templates의 hello.html 을 찾아감
     }
