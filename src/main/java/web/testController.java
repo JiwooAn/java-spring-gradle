1abd7fd3a1db6ea76946334d7366075251ff8b47
JiwooAn <jiyouan9@gmail.com>
2022-10-14 14:37:22 +0900

    feat: api 추가

--- src/main/java/web/testController.java
+++ src/main/java/web/testController.java
@@ -3,6 +3,7 @@ package web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import service.TestService;
@@ -27,4 +28,37 @@ public class testController {
         return "hello"; // templates의 hello.html 을 찾아감
     }
 
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello-string " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        
+    }
 }
